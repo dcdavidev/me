0e83fe24a1ad2fad112da4c45f3447828fd14534
Davide Di Criscito <dcdavidev@gmail.com>
2026-01-30 07:44:09 +0100

    fix: api baseUrl

--- packages/web-configs/src/api.ts
+++ packages/web-configs/src/api.ts
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.DEV
-  ? `http://localhost:${import.meta.env.PORT}/api`
-  : `https://${import.meta.env.HOST}/api`;
+const devURL = `http://localhost:${import.meta.env.PORT || 3000}/api`;
+const prodURL = import.meta.env.HOST
+  ? `https://${import.meta.env.HOST}/api`
+  : 'https://dcdavidev.me/api';
+
+const baseURL = import.meta.env.DEV ? devURL : prodURL;
+
+console.log(`[Axios Init] Mode: ${import.meta.env.MODE} | BaseURL: ${baseURL}`);
+
+console.log(`[Axios Init] Mode: ${import.meta.env.MODE}, BaseURL: ${baseURL}`);
 
 export const api = axios.create({
   baseURL: baseURL,
   timeout: 10_000,
-  withCredentials: true,
 });
--- packages/web-configs/src/vite-env.d.ts
+++ packages/web-configs/src/vite-env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly PORT: string;
   readonly DEV: boolean;
   readonly HOST: string;
+  readonly MODE: string;
 }
 
 interface ImportMeta {
